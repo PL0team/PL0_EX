5936d8a9adcc48aa14bf1454220b78c624e8a808
Administrator <Administrator@PC-20160717ETBD>
2017-09-16 16:37:02 +0800

    sync

---
+++ PL0_exp/PL0_exp/PL0_exp.cpp
@@ -0,0 +1,14 @@
+// PL0_exp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world!" << endl;
+    return 0;
+}
+
